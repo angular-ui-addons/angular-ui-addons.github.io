89d177d65eca6550d342f3d8a49570583ea4f556
Denis Ilguzin <ilguzin@gmail.com>
2014-01-14 11:27:30 +0400

    Initial commit of inclist directive description

---
+++ js/inclist.js
@@ -0,0 +1,16 @@
+angular.module('testApp', ['angular-ui-addons.inclist'])
+
+    .controller('InclistTestCtrl', ["$scope", function ($scope) {
+
+      // This to be extended
+      $scope.list = [
+        {id: "1", name: "Elephant"},
+        {id: "2", name: "Monkey"},
+        {id: "3", name: "Tiger"},
+        {id: "4", name: "Zebra"}
+      ];
+
+      // Dunno why but this may be the "main's" scope variable
+      $scope.propSelection = "";
+
+    }]);
